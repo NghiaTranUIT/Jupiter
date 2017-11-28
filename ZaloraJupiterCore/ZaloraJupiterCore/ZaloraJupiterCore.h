dcd88af0475e196beccb485c43d9f5d028f2f765
Nghia Tran <nghia.tran@zalora.com>
2017-11-28 16:52:15 +0700

    Init JupiterCore

---
+++ ZaloraJupiterCore/ZaloraJupiterCore/ZaloraJupiterCore.h
@@ -0,0 +1,19 @@
+//
+//  ZaloraJupiterCore.h
+//  ZaloraJupiterCore
+//
+//  Created by NghiaTran on 11/28/17.
+//  Copyright © 2017 Zalora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZaloraJupiterCore.
+FOUNDATION_EXPORT double ZaloraJupiterCoreVersionNumber;
+
+//! Project version string for ZaloraJupiterCore.
+FOUNDATION_EXPORT const unsigned char ZaloraJupiterCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZaloraJupiterCore/PublicHeader.h>
+
+
